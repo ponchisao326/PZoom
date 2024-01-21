e71883c6f707f1dd3a63d2ac88048f067d8c1ccf
ponchisao326_ <ponchisao326@hotmail.com>
2024-01-21 13:09:16 +0100

    Initial commit

---
+++ src/main/java/ponchisaohosting/xyz/pzoom/PZoom.java
@@ -0,0 +1,15 @@
+package ponchisaohosting.xyz.pzoom;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PZoom implements ModInitializer {
+    public static final String MOD_ID = "pzoom";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("PZOOM: Initiating...");
+    }
+}
---
+++ src/main/java/ponchisaohosting/xyz/pzoom/client/PZoomClient.java
@@ -0,0 +1,11 @@
+package ponchisaohosting.xyz.pzoom.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PZoomClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
