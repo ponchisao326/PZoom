4a46d70ca730947528fcba8d2c88272c0d6e1560
ponchisao326_ <ponchisao326@hotmail.com>
2024-01-21 17:43:42 +0100

    Added a button to connect to a custom server

--- src/main/java/ponchisaohosting/xyz/pzoom/mixin/TitleScreenMixin.java
+++ src/main/java/ponchisaohosting/xyz/pzoom/mixin/TitleScreenMixin.java
@@ -4,24 +4,43 @@ import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.network.ServerAddress;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+
+import java.util.List;
 
 @Mixin(TitleScreen.class)
-public abstract class TitleScreenMixin extends Screen {
+public class TitleScreenMixin extends Screen {
+
+    // Variables para la conexión directa al servidor
+    private static final String SERVER_ADDRESS = "node-marb.ponchisaohosting.xyz:25565";
+    private static final String SERVER_NAME = "Blanchiniiland 3";
+    private static final String CONNECT_BUTTON_TEXT = "Conectar";
+    private static final int BUTTON_WIDTH = 200;
+    private static final int BUTTON_HEIGHT = 20;
 
     protected TitleScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(at = @At("RETURN"), method = "initWidgetsNormal")
+    @Inject(method = "initWidgetsNormal", at = @At(value = "TAIL"))
     private void addCustomButton(int y, int spacingY, CallbackInfo ci) {
-        this.addDrawableChild(ButtonWidget.builder(Text.of("Prueba"), button -> {
-            this.client.setScreen(new MultiplayerScreen(this));
-        }).dimensions(this.width / 2 - 100 + 210, y, 50, 20).build());
+        int offsetX = 0;
+
+        ServerInfo info = new ServerInfo(I18n.translate("selectServer.defaultName"), SERVER_ADDRESS, false);
+        info.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.PROMPT);
+
+        var button = new ButtonWidget.Builder(Text.of(CONNECT_BUTTON_TEXT), (buttonWidget) -> {
+            ConnectScreen.connect(this, this.client, ServerAddress.parse(SERVER_ADDRESS), info);
+            buttonWidget.playDownSound(this.client.getSoundManager());
+        }).dimensions(this.width / 2 - 100 + offsetX, y, BUTTON_WIDTH, BUTTON_HEIGHT).build();
+
+        this.addDrawableChild(button);
     }
 }
